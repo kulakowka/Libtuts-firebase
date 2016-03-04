212f60466909c30e2c72577c84fb7c81bc109f99
kulakowka <kulakowka@gmail.com>
2016-03-05 01:51:09 +0300

    platforms seed complete

--- seeders/languages/index.js
+++ seeders/languages/index.js
@@ -2,24 +2,11 @@
 
 var debug = require('debug')('app:seed:languages')
 var ref = require('../../utils/firebase')
-var data = require('./dataObj')
+var data = require('./data')
 
-// Save data to firebase
 ref
 .child('languages')
 .set(data)
 .then(() => debug('seed complete'))
 .then(() => process.exit(0))
 .catch(console.log)
-
-// Это оставим на время разработки
-// data = data.reduce((data, name) => {
-//   const id = name.toLowerCase().replace(/\s/g, '-').replace(/\#/g, 'sharp').replace(/\+/g, 'p')
-//   data[id] = {
-//     name,
-//     tutorialsCount: 0,
-//     projectsCount: 0
-//   }
-//   return data
-// }, {})
-// console.log(JSON.stringify(data, null, '\t'))
--- seeders/platforms/index.js
+++ seeders/platforms/index.js
@@ -1,26 +1,12 @@
 'use strict'
 
-var debug = require('debug')('seed:platforms')
-var assert = require('assert')
-var firebase = require('../../utils/firebase')
+var debug = require('debug')('app:seed:platforms')
+var ref = require('../../utils/firebase')
 var data = require('./data')
 
-// Prepare data in the right format
-data = data.reduce((data, name) => {
-  const id = name.toLowerCase().replace(/\s/g, '-').replace(/\#/g, 'sharp').replace(/\+/g, 'p')
-  data[id] = {
-    name,
-    projectsCount: 0
-  }
-  return data
-}, {})
-
-// Save data to firebase
-firebase.set('platforms', data, (err, result) => {
-  assert.ifError(err)
-  assert.ok(result)
-
-  const count = Object.keys(result).length
-
-  debug(`${count} platforms has been set`)
-})
+ref
+.child('platforms')
+.set(data)
+.then(() => debug('seed complete'))
+.then(() => process.exit(0))
+.catch(console.log)

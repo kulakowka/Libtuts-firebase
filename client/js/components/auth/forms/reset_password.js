6f6d818f8a7dccebf17b37021c4aaec2705ae372
kulakowka <kulakowka@gmail.com>
2016-03-06 09:05:59 +0300

    reset password complete

--- client/js/components/auth/forms/reset_password.js
+++ client/js/components/auth/forms/reset_password.js
@@ -4,14 +4,20 @@ import User from '../../../models/User'
 const Form = {
   controller (args) {
     this.user = m.prop(new User())
-    this.resetPassword = function (user, event) {
+    this.result = m.prop(false)
+    this.error = m.prop(false)
+    this.resetPassword = (user, event) => {
       event.preventDefault()
       User.resetPassword({
         email: user.email()
       }).then(() => {
-        console.log('Pasword reset successfully')
-        m.route('/')
+        this.result('A temporary password has been sent to your email. If it doesn\'t appear within a few minutes, check your spam folder.')
+        m.redraw()
       }).catch((err) => {
+        if (err.code === 'INVALID_USER') {
+          this.error('Can\'t find that email, sorry.')
+          m.redraw()
+        }
         console.log('Pasword reset failed', err)
       })
     }
@@ -22,16 +28,26 @@ const Form = {
 
     return (
       <section>
-        <form class='defaultForm' onsubmit={ctrl.resetPassword.bind(this, user)}>
+        {ctrl.result() ? (
+          <div>{ctrl.result()}</div>
+        ) : (
+          <form class='defaultForm' onsubmit={ctrl.resetPassword.bind(this, user)}>
 
-          <div class='field'>
-            <input type='text' oninput={m.withAttr('value', user.email)} value={user.email()} placeholder='Email' required autocomplete='off'/>
-          </div>
+            <div class='field-description'>
+              Enter your email address and we will send you temporary password with which within 24 hours you will be able to log in and update your account.
+            </div>
 
-          <div class='field buttons center'>
-            <button class='btn btn-action btn-wide' type='submit'>Reset password</button>
-          </div>
-        </form>
+            <div class={ctrl.error() ? 'field field-with-error' : 'field'}>
+              <input type='text' oninput={m.withAttr('value', user.email)} value={user.email()} placeholder='Email' required autocomplete='off'/>
+            </div>
+
+            {!ctrl.error() ? null : <div class='field-error'>{ctrl.error()}</div>}
+
+            <div class='field buttons center'>
+              <button class='btn btn-action btn-wide' type='submit'>Reset password</button>
+            </div>
+          </form>
+        )}
       </section>
     )
   }
--- client/js/pages/auth/reset_password.js
+++ client/js/pages/auth/reset_password.js
@@ -6,8 +6,8 @@ const ResetPassword = {
   view (ctrl) {
     return (
       <div class='row'>
-        <div class='col col-4'>
-          <h1>Reset password</h1>
+        <div class='col col-4 col-l'>
+          <h1>Reset your password</h1>
           {m.component(Form)}
           <p>New to LibTuts? <a href={helpers.signUpUrl()} config={m.route}>Create an account</a>.</p>
         </div>
